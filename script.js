817507dd0ef870191e6051b8e59bdd553eef8c95
Dabeey <abbasdaughter18@gmail.com>
2025-06-24 18:30:50 +0100

    Initialize clock script to retrieve and display current time

---
+++ script.js
@@ -0,0 +1,8 @@
+let hrs = document.getElementById('hrs');
+let mins = document.getElementById('mins');
+let secs = document.getElementById('secs');
+
+let currentTime = new Date();
+let hours = currentTime.getHours();
+let minutes = currentTime.getMinutes();
+let seconds = currentTime.getSeconds();

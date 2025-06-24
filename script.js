324e1c1edee5a347efe4a020698a6f10ad85ceee
Dabeey <abbasdaughter18@gmail.com>
2025-06-24 18:42:39 +0100

    Fix variable names for minutes and seconds in clock script

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let hrs = document.getElementById('hrs');
-let mins = document.getElementById('mins');
-let secs = document.getElementById('secs');
+let min = document.getElementById('min');
+let sec = document.getElementById('sec');
 
-let currentTime = new Date();
-hrs.innerHTML = currentTime.getHours();
-min.innerHTML = currentTime.getMinutes();
-sec.innerHTML = currentTime.getSeconds();
+// Update every second
+setInterval(() => {
+    let currentTime = new Date();
+    hrs.innerHTML = currentTime.getHours() < 10 ? '0' + currentTime.getHours() : currentTime.getHours();
+    min.innerHTML = currentTime.getMinutes() < 10 ? '0' + currentTime.getMinutes() : currentTime.getMinutes();
+    sec.innerHTML = currentTime.getSeconds() < 10 ? '0' + currentTime.getSeconds() : currentTime.getSeconds();
+}, 1000);

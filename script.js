ef77421fa4cd3386f2bf658a29aa9fac4feb5998
Dabeey <abbasdaughter18@gmail.com>
2025-06-24 18:33:47 +0100

    Fix variable names in time display and update content for minutes and seconds

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let mins = document.getElementById('mins');
 let secs = document.getElementById('secs');
 
 let currentTime = new Date();
-let hours = currentTime.getHours();
-let minutes = currentTime.getMinutes();
-let seconds = currentTime.getSeconds();
+hrs.innerHTML = currentTime.getHours();
+min.innerHTML = currentTime.getMinutes();
+sec.innerHTML = currentTime.getSeconds();
